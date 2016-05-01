218a50fdcb976778f5a5afbba4c1eb58d46eef75
chotu24 <anandk.kumar506@gmail.com>
2016-04-30 21:00:38 -0400

    made service request to display id and image

--- js/patterns/taskManager.js
+++ js/patterns/taskManager.js
@@ -11,10 +11,44 @@
 	TaskApp.controller('taskCtrl',taskController);
 
 /*var TaskApp.factory('Task', function($scope){
-
 	$scope.message = "test";
-
 })
 */
+/*
+.controller('basicHttpController',function ($scope, $http){
+	$scope.getName = function(){
+		var futureResponse = $http.get('data.json');
+		futureResponse.success(function ( data,status,
+*/
+	var httpRequestController = function($scope, $http){
+		var self = this;
+		function onUserComplete(response){
+			var data = response.data;
+			$scope.id = data.id;
+			$scope.myLogo = data.avatar_url;
+			console.log($scope.id);
+		}
+		function error(e){
+			console.log(e);
+		}
+		$scope.getUser = function(userToSearch){
+				console.log(userToSearch);
+				$http.get("https://api.github.com/users/"+userToSearch).
+					then(onUserComplete, error);
 
+		};
+		console.log('inside httpRequestController app');
+	}
+	TaskApp.controller('taskCtrl2', httpRequestController);
 }());
+
+
+
+
+
+
+
+
+
+
+
